5d31302e17e284a4c1bfd3c08ae25442ad3c706b
Chukwuemeka Benson Onunkwo <bconunkwo@gmail.com>
2021-09-27 11:47:33 +0100

    Adding to GIT Version Control
    Committing to Version Control system to enable continuous deployment to Azure app services.

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,7 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+logging.basicConfig(level=logging.DEBUG, format=f'%(asctime)s ::--:: %(message)s')
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
