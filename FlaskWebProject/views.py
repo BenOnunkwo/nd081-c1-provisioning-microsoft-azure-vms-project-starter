7bb9e615b0f26efada672b0f669c260b921ba1df
Chukwuemeka Benson Onunkwo <bconunkwo@gmail.com>
2021-09-27 18:17:54 +0100

    Updated AUTH URL

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -135,7 +135,4 @@ def _build_msal_app(cache=None, authority=None):
     
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    return _build_msal_app(authority=authority).get_authorization_request_url(
-        scopes or [],
-        state=state or str(uuid.uuid4()),
-        redirect_uri=url_for("authorized", _external=True))
+    return "https://flaskcms.azurewebsites.net/.auth/login/aad/callback"
